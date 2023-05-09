c63bf127f339005a0b93f5041d85f051f0797074
madhuachuth <madhuachuth7@gmail.com>
2023-05-09 09:24:08 +0000

    login

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+// Write your code here
+import './index.css'
+
+const Login = props => {
+  const {loggedIn} = props
+  return (
+    <button type="button" onClick={loggedIn}>
+      Login
+    </button>
+  )
+}
+
+export default Login
---
+++ src/components/Logout/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+
+const Logout = props => {
+  const {loggedOut} = props
+
+  return (
+    <button type="button" onClick={loggedOut}>
+      Logout
+    </button>
+  )
+}
+
+export default Logout
